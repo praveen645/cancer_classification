9cff4b1c31fd6314ea67781faf41f68efb65ef0f
praveen645 <31787053+praveen645@users.noreply.github.com>
2019-03-28 01:04:05 +0530

    Add files via upload
    a basic ml model that uses naive bayes classifier.

---
+++ ml_project.py
@@ -0,0 +1,27 @@
+from sklearn.naive_bayes import GaussianNB
+import pandas as pd
+import numpy as np
+import pickle
+
+cancer_ds=pd.read_csv('cervical_cancer_replaced.csv')
+print (cancer_ds.shape)
+
+feature=cancer_ds.iloc[:,:9]
+target=cancer_ds.iloc[:,9]
+
+
+gb=GaussianNB()
+gb.fit(feature,target)
+
+
+file = open('cancer.pickle','wb')
+pickle.dump(gb,file)
+
+print(" *************************************************************************************** ")
+
+print(" *************************************************************************************** ")
+
+file = open('cancer.pickle','rb')
+gb1 = pickle.load(file)
+
+print(gb1.predict([[34,2,0,0,1,0,1,1,1]]))
